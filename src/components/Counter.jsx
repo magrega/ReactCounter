18089f05d6c9372670f350e4f1a7471d2e415c69
magrega <proshanov@yandex.ru>
2023-03-04 13:34:49 +0300

    polished the whole thing

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,40 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const Counter = ({counter}) => {
+const Counter = ({ counter }) => {
 
     const [count, setCount] = useState(counter);
     const [text, setText] = useState('');
 
-    function increment() {
-        if (count % 10 === 0 && count > 0) {
-            setCount(count => count + 1);
-            setText(`You've reached ${count}`);
-
-        } else {
-            setCount(count => count + 1);
-            setText(``);
-        }
+    const increment = () => {
+        setCount(count + 1);
     }
 
-    function decrement() {
-        setCount(count => count - 1);
+    const decrement = () => {
+        setCount(count - 1);
     }
 
-    function reset() {
+    const reset = () => {
         setCount(0);
     }
 
-        return (
-            <div className="app">
-                <div className="counter">{count} {text}</div>
-                <div className="controls">
-                    <button onClick={increment}>INC</button>
-                    <button onClick={decrement}>DEC</button>
-                    <button onClick={reset}>RESET</button>
-                </div>
+    useEffect(() => {
+        count % 10 === 0 && count > 0 ? setText(`You've reached `) : setText(``);
+    }, [count])
+
+    return (
+        <>
+            <div className="counter">{text} {count}</div>
+            <div className="controls">
+                <button onClick={increment}>INC</button>
+                <button onClick={decrement}>DEC</button>
+                <button onClick={reset}>RESET</button>
             </div>
-        )
-    
+        </>
+    )
+
 }
 export default Counter;
 
